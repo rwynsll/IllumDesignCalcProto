e5134aa669f50c1b837f5febf10201690b8999bb
eros niko alvarez <iamerosniko@yahoo.com>
2018-01-07 16:35:02 +0800

    init creating class for formulas

---
+++ calc/Formulas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calc
+{
+    public class Formulas
+    {
+
+        //compute 3D point by point in metric sys
+        public void MetricSystem3D()
+        {
+            var a = 1 + 1;
+
+        }
+    }
+}
