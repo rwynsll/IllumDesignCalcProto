db2288f296387e7a5d8116d10392559d0c33aa21
eros niko alvarez <iamerosniko@yahoo.com>
2018-01-28 17:35:29 +0800

    create functions for computation
    initial plan

--- calc/Formulas.cs
+++ calc/Formulas.cs
@@ -8,12 +8,66 @@ namespace calc
 {
     public class Formulas
     {
+        private double _lumens;
+        private double _area;
+        private double _cu;
+        private double _mf;
+        private double _transmissionFactor;
+        private double _numberOfLamps;
+
 
         //compute 3D point by point in metric sys
         public void MetricSystem3D()
         {
             var a = 1 + 1;
+        }
+
+        public double Illuminance(bool isMetric)
+        {
+            double result = 0.0;
+            if (isMetric)
+            {
+
+            }
+            else
+            {
+
+            }
+
+            return result;
+        }
+
+
+
+        #region metric_systems
+
+        private double IlluminationMetric()
+        {
+            double result = 0;
+
+
+
+
+            return result;
 
         }
+
+        private double LuminationMetric()
+        {
+            double result = 0;
+
+
+
+
+            return result;
+
+        }
+
+
+        #endregion
+
+        #region english_systems
+        #endregion
     }
+
 }
